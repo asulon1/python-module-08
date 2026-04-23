526b3696757387751ecec9de88216ee2ddc66fe1
asulon <asulon@student.42.fr>
2026-04-23 18:50:09 +0200

    init ex0

---
+++ ex0/construct.py
@@ -0,0 +1,35 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  construct.py                                      :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/04/23 16:25:35 by asulon          #+#    #+#               #
+#  Updated: 2026/04/23 18:44:51 by asulon          ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+import sys
+import os
+
+def main():
+    print("MATRIX STATUS: You're still plugged in")
+    print("Current Python:")  #/usr/bin/python3.11
+    print("Virtual Environment:") # None detected
+    print("# WARNING: You're in the global environment!" \
+        "The machines can see everything you install.")
+    print("To enter the construct, run: \n " \
+    "python -m venv matrix_env\n" \
+    "source matrix_env/bin/activate # On Unix\n" \
+    "matrix_env\Scripts\activate # On Windows\n")
+    
+    print("Then run this program again")
+    print("dede")
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
