b01c204d46fcd2d98de9f870f9d42072dcd2be35
asulon <adrien.sulon@epitech.eu>
2026-04-24 17:50:08 +0200

    ex0 update with new subject

--- ex0/construct.py
+++ ex0/construct.py
@@ -3,10 +3,10 @@
 #                                                      :::      ::::::::    #
 #  construct.py                                      :+:      :+:    :+:    #
 #                                                  +:+ +:+         +:+      #
-#  By: asulon <asulon@student.42.fr>             +#+  +:+       +#+         #
+#  By: asulon <asulon@student.42nice.fr>         +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/04/23 16:25:35 by asulon          #+#    #+#               #
-#  Updated: 2026/04/24 16:33:04 by asulon          ###   ########.fr        #
+#  Updated: 2026/04/24 17:48:50 by asulon          ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -28,16 +28,21 @@ def main():
     if not is_virtual:
         print("\nWARNING: You're in the global environment!\n"
               "The machines can see everything you install.\n")
-        print("To enter the construct, run: \n "
+        print("To enter the construct, run:\n"
               "python -m venv matrix_env\n"
               "source matrix_env/bin/activate # On Unix\n"
               "matrix_env/Scripts/activate # On Windows\n")
         print("Then run this program again")
     else:
-        print(f"Environment Path: {os.environ.get('VIRTUAL_ENV')}\n")
+        for path in sys.path:
+            if "site-packages" in path:
+                package_path = path
+                break
+
+        print(f"Environment Path: {sys.prefix}\n")
         print("SUCCESS: You're in an isolated environment!\n"
               "Safe to install packages without affecting the global system\n")
-        print(f"Package installation path: {os.environ}\n")
+        print(f"Package installation path: {package_path}\n")
 
 
 if __name__ == "__main__":
